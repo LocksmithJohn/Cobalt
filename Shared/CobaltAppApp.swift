b3e53d4cb524129aa88370fc7b9c74f17607b2a1
LocksmithJohn <slusarzjan@gmail.com>
2022-02-12 09:21:37 +0100

    first commit

---
+++ Shared/CobaltAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobaltAppApp.swift
+//  Shared
+//
+//  Created by Jan Slusarz on 12/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CobaltAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
